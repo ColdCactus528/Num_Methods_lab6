2782dba5996aa8fbbaa268c4ae1395d50dcc788f
ColdCactus528 <56156478+ColdCactus528@users.noreply.github.com>
2021-12-19 19:13:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,31 +2,31 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-a1 = 3
-a2 = 2
+a1 = 2
+a2 = 3
 b1 = -1
 b2 = 1
-c1 = 2
-c2 = 3.2391
+c1 = -1
+c2 = 8.064715260291829
 
 def p(x):
-    return math.cos(x)
+    return math.tan(x)
 
 def q(x):
-    return math.sin(x)
+    return -2*x / math.cos(x)
 
 def f(x):
-    return math.sin(x) * x
+    return 2 - 2*x*x*x / math.cos(x)
 
 def ExactSolution(x):
-    return x + math.cos(x)
+    return x*x + math.sin(x)
 
-def SweepMethod(min, max, step, flag):
-    massX = np.arange(min, max+step/2, step)
+def SweepMethod(min, max, n, flag):
+    massX = np.linspace(min, max, n)
     massP = []
     massQ = []
     massF = []
-    n = len(massX)
+    step = (max - min) / (n-1)
 
     for x in massX:
         massP.append(p(x))
@@ -41,9 +41,9 @@ def SweepMethod(min, max, step, flag):
 
     if flag != 0:
         massA = [0]
-        massB = [-2 + 2*step*a1/b1 + a1*step*step*massP[0]/b1 + massQ[0]*step*step]
+        massB = [-2 + 2*step*a1/b1 - a1*step*step*massP[0]/b1 + massQ[0]*step*step]
         massC = [2]
-        massD = [massF[0]*step*step + c1*2*step/b1 - step*step*c1/b1]
+        massD = [massF[0]*step*step - c1*step*step*massP[0]/b1 + 2*step*c1/b1]
 
     for i in range(1, n-1):
         massA.append(1 - step*massP[i]/2)
@@ -61,7 +61,7 @@ def SweepMethod(min, max, step, flag):
         massA.append(2)
         massB.append(-2 - 2*step*a2/b2 - massP[-1]*step*step*a2/b2 + massQ[-1]*step*step)
         massC.append(0)
-        massD.append(massF[-1]*step*step + step*step*massP[-1]*c2/b2 - 2*step*c2/b2)
+        massD.append(massF[-1]*step*step - step*step*massP[-1]*c2/b2 - 2*step*c2/b2)
 
     massA3 = [-massC[0] / massB[0]]
     massB3 = [massD[0] / massB[0]]
@@ -72,38 +72,44 @@ def SweepMethod(min, max, step, flag):
 
     massResultY = [massB3[-1]]
     for i in range(n - 2, -1, -1):
-        massResultY.append(massB3[i] + massA3[i]*massResultY[len(massX) - 2 - i])
+        massResultY.append(massB3[i] + massA3[i]*massResultY[n - 2 - i])
 
-    massX = np.arange(min, max+step/2, step)
-    print(massResultY[::-1])
     return massX, massResultY[::-1]
 
-def LogError(min, maximum, Func):
-    step = 0.1
+def LogError(min, maximum, n, Func, flag):
     massErr = []
     massStep = []
-    while(step > 1e-4):
-        massCalcX, massCalcY = Func(min, maximum, step)
-        massTrue = [ExactSolution(i) for i in np.arange(min, max+step/2, step)]
+    mass1 = []
+    mass2 = []
+    while(n < 1000):
+        massCalcX, massCalcY = Func(min, maximum, n, flag)
+        massTrue = [ExactSolution(i) for i in np.linspace(min, maximum, n)]
 
-        print(len(massTrue))
-        print(len(massCalcY))
+        print(massTrue)
+        print(massCalcY)
         maxErr = 0
         for i in range(len(massTrue)):
             if maxErr < abs(massCalcY[i] - massTrue[i]):
                 maxErr = abs(massCalcY[i] - massTrue[i])
 
+        step = (maximum - min) / (n - 1)
+        mass1.append(step)
+        mass2.append(maxErr)
         massStep.append(math.log(step))
         massErr.append(math.log(maxErr))
-        step = step / 10
+        n = n * 2
 
-    return massStep, massErr
+    return massStep, massErr, mass1, mass2
 
 step = 0.05
+n = 21
 min = 0
 max = 1
-massX, massY = SweepMethod(min, max, step, 1)
-# massStep, massErr = LogError(min, max, SweepMethod)
+massX, massY = SweepMethod(min, max, n, 0)
+massStep, massErr, massStepNoLog, massErrNoLog = LogError(min, max, n, SweepMethod, 1)
+print(massErrNoLog)
+print()
+print(massStepNoLog)
 plt.title("19 задание")  # заголовок/
 plt.xlabel("x")
 plt.ylabel("y")
